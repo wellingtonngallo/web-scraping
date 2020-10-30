ca488a750f56292233694d991fbec8cd70f66579
Wellington Gallo <wellingtongallo@MacBook-Pro-de-Wellington.local>
2020-10-29 21:58:40 -0300

    ajuste finais

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const puppeteer = require('puppeteer');
 const ObjectsToCsv = require('objects-to-csv');
 const googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyAUy0GrZ_6zaaHmOyb6nDWtygojgFO4U2A'
+    key: 'YOUR_KEY',
+    Promise: Promise
 });
-
 
 async function getData() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     const url = `https://www.zapimoveis.com.br/venda/imoveis/sc+joinville++costa-e-silva/?onde=,Santa%20Catarina,Joinville,,Costa%20E%20Silva,,,neighborhood,BR%3ESanta%20Catarina%3ENULL%3EJoinville%3EBarrios%3ECosta%20E%20Silva,-26.274768,-48.854485%3B,Santa%20Catarina,Joinville,,Sagua%C3%A7%C3%BA,,,neighborhood,BR%3ESanta%20Catarina%3ENULL%3EJoinville%3EBarrios%3ESaguacu,-26.271418,-48.822472%3B,Santa%20Catarina,Joinville,,Am%C3%A9rica,,,neighborhood,BR%3ESanta%20Catarina%3ENULL%3EJoinville%3EBarrios%3EAmerica,-26.284252,-48.850051%3B,Santa%20Catarina,Joinville,,Bom%20Retiro,,,neighborhood,BR%3ESanta%20Catarina%3ENULL%3EJoinville%3EBarrios%3EBom%20Retiro,-26.283145,-48.840876%3B,Santa%20Catarina,Joinville,,Floresta,,,neighborhood,BR%3ESanta%20Catarina%3ENULL%3EJoinville%3EBarrios%3EFloresta,-26.337835,-48.847484&transacao=Venda&tipo=Im%C3%B3vel%20usado`;
     const data = [];
-    const latAndLong = [];
+    
     let count = 1;
     
     await page.goto(url);
     await page.waitForSelector('.gutter-top-double.simple-card__box');
 
-    while(count < 2) {
+    while(count < 10) {
         count++;
 
         const result = await page.$$eval('.gutter-top-double.simple-card__box', response => {
@@ -30,10 +30,10 @@ async function getData() {
 
                 return prop = {
                     price: price ? price.innerText : 'Não definido',
-                    address: address ? address.innerText : 'Não definido',
+                    address: address ? decodeURI(address.innerText) : 'Não definido',
                     area: area ? area.innerText.match(/\d+/g)[0] : 'Não definido',
-                    bedrooms: bedrooms ? bedrooms.innerText : 'Não definido',
-                    parkingSpaces: parkingSpaces ? parkingSpaces.innerText : 'Não definido',
+                    bedrooms: bedrooms ? bedrooms.innerText.match(/\d+/g)[0] : 'Não definido',
+                    parkingSpaces: parkingSpaces ? parkingSpaces.innerText.match(/\d+/g)[0] : 'Não definido',
                     bathrooms: bathrooms ? bathrooms.innerText : 'Não definido'
                 };
             });
@@ -42,30 +42,30 @@ async function getData() {
         data.push(...result);
        
         await page.click(`a[href='?pagina=${count}`);
-        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 });
     }
 
-    function resolvePromisseGeocode(address) {
-        return new Promise(function(resolve, reject) {
-            googleMapsClient.geocode({address: address}, function(err, res) {
-                resolve(res.json.results[0].geometry.location);
-            });
-        });
+   async function resolvePromisseGeocode(address) {
+        return googleMapsClient.geocode({address: address}).asPromise().then(response => response.json.results[0]);
     }
 
+    const latAndLong = [];
     data.forEach(item => {
         latAndLong.push(resolvePromisseGeocode(item.address));
     });
 
-    Promise.all(latAndLong).then(function(response) {
+    Promise.all(latAndLong).then(response => {
         response.map((item, index) => {
-            data[index].lat = item.lat;
-            data[index].lng = item.lng;
+            if (item) {
+                data[index].lat = item.geometry.location.lat.toString();
+                data[index].lng = item.geometry.location.lng.toString();
+            }
         });
         
         const csv = new ObjectsToCsv(data);
 
         csv.toDisk('./data.csv');
+        
    });
 
     await browser.close();
